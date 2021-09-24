aa2a89c1353eced30044c250d1b01e292e36fc54
Irina <irina@netmagik.com>
2021-09-24 12:18:02 -0400

    Break up Weather and helpers into its own components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import Weather from './Weather';
 
 const api = {
   key: '0f98c8f94399873693a034fcff6a4818',
@@ -22,18 +23,6 @@ function App() {
     }
   }
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
-  }
-
   return (
     <div className={
       (typeof weather.main != 'undefined') 
@@ -52,34 +41,7 @@ function App() {
         onKeyPress={search} />
      </div>
 
-{(typeof weather.main != 'undefined') ? (
-  <div>
-  <div className="location-box">
-    <div className="location">
-      {weather.name}, {weather.sys.country}
-      <div className="date">
-       {dateBuilder(new Date())}
-      </div>
-    </div>
-  </div>
-  <div className="weather-box">
-    <div className="temp">
-      {Math.round((weather.main.temp) * 9 / 5 + 32)}°F
-    </div>
-    <div className="highlow">
-      <div className="high">
-        H: {Math.round((weather.main.temp_max) * 9 / 5 + 32)}°F
-      </div>
-      <div className="low">
-        L: {Math.round((weather.main.temp_min) * 9 / 5 + 32)}°F
-      </div>
-    </div>
-    <div className="weather">
-      {weather.weather[0].main}
-    </div>
-  </div>
-  </div>
-  ) : ('')}
+<Weather weather={weather}/>
 
       </main>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { dateBuilder } from './helpers';
+
+const Weather = (props) => (
+    (typeof props.weather.main != 'undefined') 
+    ? ( <div>
+        <div className="location-box">
+          <div className="location">
+            {props.weather.name}, {props.weather.sys.country}
+            <div className="date">
+             {dateBuilder(new Date())}
+            </div>
+          </div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            {Math.round((props.weather.main.temp) * 9 / 5 + 32)}°F
+          </div>
+          <div className="highlow">
+            <div className="high">
+              H: {Math.round((props.weather.main.temp_max) * 9 / 5 + 32)}°F
+            </div>
+            <div className="low">
+              L: {Math.round((props.weather.main.temp_min) * 9 / 5 + 32)}°F
+            </div>
+          </div>
+          <div className="weather">
+            {props.weather.weather[0].main}
+          </div>
+        </div>
+        </div>
+        ) : ('')
+)
+
+export default Weather;
