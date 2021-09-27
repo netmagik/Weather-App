f3fc56c48a2370791f6a8d0e2602c1f95c7e732b
Irina <irina@netmagik.com>
2021-09-27 16:08:32 -0400

    style forecast items

--- src/forecast.js
+++ src/forecast.js
@@ -5,6 +5,33 @@ const Forecast = (props) => {
        <>
     <h1>Forecast</h1>
     <button id="forecast" onClick={props.getForecast}>5 Day Forecast</button>
+    <ul className="forecast">
+        <li className="forecast-day">
+            <span class="forecast-temp">Temp</span>
+            <span class="forecast-date">Date</span>
+            <span class="forecast-desc">Description</span>
+        </li>
+        <li className="forecast-day">
+            <span class="forecast-temp">Temp</span>
+            <span class="forecast-date">Date</span>
+            <span class="forecast-desc">Description</span>
+        </li>
+        <li className="forecast-day">
+            <span class="forecast-temp">Temp</span>
+            <span class="forecast-date">Date</span>
+            <span class="forecast-desc">Description</span>
+        </li>
+        <li className="forecast-day">
+            <span class="forecast-temp">Temp</span>
+            <span class="forecast-date">Date</span>
+            <span class="forecast-desc">Description</span>
+        </li>
+        <li className="forecast-day">
+            <span class="forecast-temp">Temp</span>
+            <span class="forecast-date">Date</span>
+            <span class="forecast-desc">Description</span>
+        </li>
+    </ul>
     </>
    )
 }
