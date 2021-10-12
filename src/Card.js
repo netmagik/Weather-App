8d636ce2de64d00b8256b5ee7c2882b4a9d5b49e
Irina <irina@netmagik.com>
2021-10-12 11:36:11 -0400

    Install Moment js - Use Moment.js for Date to prevent errors ('Invalid Date') on Mobile

--- src/Card.js
+++ src/Card.js
@@ -6,8 +6,11 @@ import lightningcon from './assets/lightningcon.png';
 import heavyraincon from './assets/heavyraincon.png';
 import smokecon from './assets/smokecon.png';
 import lightraincon from './assets/lightraincon.png';
+const moment = require('moment');
 
 const Card = (props) => {
+
+  
 
     // Set names for weather icons
     const weathericons = {
@@ -20,26 +23,30 @@ const Card = (props) => {
         "Snow" : snowcon
     };
    
-    const weekday = props.day;
-    const options = { 
-        weekday: 'long', 
-        year: 'numeric', 
-        month: 'long', 
-        day: 'numeric' 
-    };
+    // const weekday = props.day;
+    // const options = { 
+    //     weekday: 'long', 
+    //     year: 'numeric', 
+    //     month: 'long', 
+    //     day: 'numeric' 
+    // };
 
-    const date = new Date(weekday.dt_txt);
+    // const date = new Date(weekday.dt_txt);
+
+    let newDate = new Date();
+    const weekday = props.day.dt * 1000;
+    newDate.setTime(weekday);
 
     return (
      
         <>
-            <img src={weathericons[weekday.weather[0].main.toString()]} alt="Sun" />
-            <p>{date.toLocaleDateString(undefined, options)}</p>
+            <img src={weathericons[props.day.weather[0].main.toString()]} alt="Sun" />
+            <p>{moment(newDate).format('dddd')}</p>
             <p className="temp">
-                {props.degreeToggle === "farenheit" ? `${Math.round(weekday.main.temp)}°F`
-                    : `${Math.round((weekday.main.temp - 32) * 5/9)}°C` }
+                {props.degreeToggle === "farenheit" ? `${Math.round(props.day.main.temp)}°F`
+                    : `${Math.round((props.day.main.temp - 32) * 5/9)}°C` }
                 </p>
-            <p>{weekday.weather[0].main}</p>
+            <p>{props.day.weather[0].main}</p>
 
         </>
     )
